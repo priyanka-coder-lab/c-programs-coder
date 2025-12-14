88fa9c87c87d788f54b7d36d83173303b66f24c2
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-14 19:24:07 +0530

    Create pass_fail.c

---
+++ pass_fail.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+  int marks;
+  printf("Enter marks: ");
+  scanf("%d", &marks);
+
+  if(marks >= 35) {
+      printf("Result: Pass");
+  } else {
+      printf("Result: Fail");
+  }
+  return 0;
+}
