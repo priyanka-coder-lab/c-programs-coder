bd47e391535dbb8a65b4db915313970a918a0af0
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-14 19:20:10 +0530

    Create positive_negative.c

---
+++ positive_negative.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter number: ");
+    scanf("%d", &num);
+
+    if(num >= 0) {
+        printf("Number is Positive");
+    } else {
+        printf("Number is Negative");
+    }
+    return 0;
+}
