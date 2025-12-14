2c1183a112a79fcd3f748f70dfd93344289fc535
priyanka-coder-lab <priyankamhamane2@gmail.com>
2025-12-15 00:24:46 +0530

    Update and rename int_variable.c to int_variable.c , float_variable.c

---
+++ int_variable.c , float_variable.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()  {
+    int a = 10;
+    printf("Value of a = %d", a);
+    return 0;
+}
+
+float_variable.c
+
+    #include <stdio.h>
+
+int main() {
+    float pi = 3.14;
+    printf("Value of pi = %.2f", pi);
+    return 0:
+}
+
+char_variable.c
+
+    
